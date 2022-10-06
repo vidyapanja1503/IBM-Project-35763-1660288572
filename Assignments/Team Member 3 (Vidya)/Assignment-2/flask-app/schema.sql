e31e1dbed95f88d224daa8d9aaf1e0666c8a5610
Vidya <113756164+vidyapanja1503@users.noreply.github.com>
2022-10-06 18:27:39 +0530

    Assignments

---
+++ Assignments/Team Member 3 (Vidya)/Assignment-2/flask-app/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+INSERT INTO users (username, password, name, email) VALUES (
+    "atp",
+    "vidya@123",
+    "dora",
+    "dora@gmail.com"
+);
