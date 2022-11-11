91418cd1766b9ccaa5ec87a692f7c04379f3bc31
Vidya <113756164+vidyapanja1503@users.noreply.github.com>
2022-11-11 21:00:27 +0530

    Assignment-3

---
+++ Assignments/Team Member 3 (Vidya)/Assignment-3/app.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask, render_template
+import ibm_boto3
+from dotenv import load_dotenv
+from ibm_botocore.client import Config, ClientError
+
+load_dotenv()
+
+COS_ENDPOINT = os.getenv("COS_ENDPOINT")
+COS_API_KEY_ID = os.getenv("COS_API_KEY_ID")
+COS_INSTANCE_CRN = os.getenv("COS_INSTANCE_CRN")
+BUCKET_NAME = "newstracker-bucket"
+
+
+cos = ibm_boto3.resource("s3",
+    ibm_api_key_id=COS_API_KEY_ID,
+    ibm_service_instance_id=COS_INSTANCE_CRN,
+    config=Config(signature_version="oauth"),
+    endpoint_url=COS_ENDPOINT
+)
+
+
+def get_bucket_contents(bucket_name):
+    print("Retrieving bucket contents from: {0}".format(bucket_name))
+    try:
+        files = cos.Bucket(bucket_name).objects.all()
+        files_names = []
+        for file in files:
+            files_names.append(file.key)
+            print("Item: {0} ({1} bytes).".format(file.key, file.size))
+        return files_names
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve bucket contents: {0}".format(e))
+
+def get_buckets():
+    print("Retrieving list of buckets")
+    try:
+        buckets = cos.buckets.all()
+        for bucket in buckets:
+            print("Bucket Name: {0}".format(bucket.name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve list buckets: {0}".format(e))
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    get_bucket_contents("newstracker-bucket")
+    return render_template('index.html', uri=COS_ENDPOINT+BUCKET_NAME)
+
